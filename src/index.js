d537f2a71486cbd73e05b15e12f19cff9826e525
Mornival <55550567+Mornival@users.noreply.github.com>
2020-10-17 17:24:36 +0300

    init: create function body

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+  let arra = [];
+  if(matrix === undefined)
+  {
+    return [];
+  }
+  for(let i = 0; i < matrix.length; i++)
+  {
+    for(let j = 0;j < matrix[i].length;j++)
+    {
+      arra.push(matrix[i][j]);
+    }
+    i++;
+    if(i < matrix.length)
+    {
+      for(let j = matrix[i].length - 1;j >= 0;j--)
+      {
+        arra.push(matrix[i][j]);
+      }
+    }
+  }
+  return arra;
 }
